3d08c1cd7ff385cb157ef33cef8cfca63fdbd102
jun low <zhijun.low@gmail.com>
2019-08-19 13:55:50 +1000

    complete exercise

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    display_answer
+  end
+
+  private
+
+  def display_answer
+    @answer = if @question.end_with?('?')
+                'Silly question, get dressed and go to work!'
+              elsif @question == 'I am going to work'
+                'Great!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
+end
